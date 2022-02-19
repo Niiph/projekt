93dd71fb3728882f600c1383086fdff2410fc8f6
Niiph <pepo.o@wp.pl>
2022-02-19 21:55:19 +0100

    expanded fixtures

--- src/DataFixtures/BreederFixtures.php
+++ src/DataFixtures/BreederFixtures.php
@@ -2,35 +2,49 @@
 
 namespace App\DataFixtures;
 
+use Faker\Factory;
 use App\Entity\Breeder;
-use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class BreederFixtures extends Fixture
 {
+    private $faker;
+    private $fakerRU;
+
+    public function __construct()
+    {
+        $this->faker = Factory::create();
+        $this->fakerRU = Factory::create('ru_RU');
+    }
+
     public function load(ObjectManager $manager): void
     {
 
-        $breeder = new Breeder();
-        $breeder->setName('S. Osipova');
-        $breeder->setOriginalName('С. Осипова');
-        $breeder->setShortcut('OS');
-        $manager->persist($breeder);
+        $breeder1 = new Breeder();
+        $breeder1->setName($this->faker->name);
+        $breeder1->setOriginalName($this->fakerRU->name);
+        $manager->persist($breeder1);
 
         $breeder2 = new Breeder();
-        $breeder2->setName('S. Repkina');
-        $breeder2->setOriginalName('С. Репкина');
-        $breeder2->setShortcut('RS');
+        $breeder2->setName($this->faker->name);
+        $breeder2->setOriginalName($this->fakerRU->name);
         $manager->persist($breeder2);
 
         $breeder3 = new Breeder();
-        $breeder3->setName('David Senk');
+        $breeder3->setName('S. Repkina');
+        $breeder3->setOriginalName('С. Репкина');
+        $breeder3->setShortcut('RS');
         $manager->persist($breeder3);
+
+        $breeder4 = new Breeder();
+        $breeder4->setName('David Senk');
+        $manager->persist($breeder4);
 
         $manager->flush();
 
-        $this->addReference('breeder_1', $breeder);
-        $this->addReference('breeder_2', $breeder2);
-        $this->addReference('breeder_3', $breeder3);
+        for ($i = 1; $i <= 4; $i++) {
+            $this->addReference('breeder_' . $i, ${'breeder' . $i});
+        }
     }
 }
--- src/DataFixtures/PlantFixtures.php
+++ src/DataFixtures/PlantFixtures.php
@@ -3,22 +3,33 @@
 namespace App\DataFixtures;
 
 use App\Entity\Plant;
-use App\Entity\Group;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class PlantFixtures extends Fixture
 {
+    private $faker;
+    private $fakerRU;
+
+    public function __construct() 
+    {
+        $this->faker = Factory::create();
+        $this->fakerRU = Factory::create('ru_RU');
+    }
     public function load(ObjectManager $manager): void
     {
         for ($i = 1; $i <= 30; $i++) {
             $plant = new Plant();
-            $plant->setName('Plant ' . $i);
-            $plant->setOriginalName('OName ' . $i);
-            $plant->setBreederId($this->getReference('breeder_' . mt_rand(1, 3)));
+            $plant->setName($this->faker->firstName);
+            if (mt_rand(0, 1) == 1) {
+                $plant->setOriginalName($this->fakerRU->firstName);
+            }
+            $plant->setBreederId($this->getReference('breeder_' . mt_rand(1, 4)));
             $plant->setGroupId($this->getReference('group_' . mt_rand(1, 4)));
             $plant->setChimera(mt_rand(0, 1));
             $plant->setImagePath($i . '.jpg');
+            $plant->setDescription($this->faker->text(200));
             $manager->persist($plant);
         }
 
